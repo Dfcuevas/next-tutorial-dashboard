d172ca3d1b4a79773f69b9ae22384b2e483d101d
Diego Cuevas <ingdcuevas@gmail.com>
2026-05-27 17:52:21 -0500

    feat/invoices, create form and validate data

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,19 +1,24 @@
+import { fetchInvoicesPages } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
 import Table from "@/app/ui/invoices/table";
 import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
-export default async function InvoicesPage(props: {
+export default async function InvoicesPage({
+    searchParams,
+}: {
     searchParams?: Promise<{
         query?: string;
         page?: string;
     }>;
 }) {
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || "";
-    const currentPage = Number(searchParams?.page) || 1;
+    const searchParamsResult = await searchParams;
+    const query = searchParamsResult?.query || "";
+    const currentPage = Number(searchParamsResult?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
 
     return (
         <div className="w-full">
@@ -31,7 +36,7 @@ export default async function InvoicesPage(props: {
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     );
